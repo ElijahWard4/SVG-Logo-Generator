5478ee8796b7a2b6167947c56bf4ec6b70cd72da
EliWard4 <elijah.ward014@gmail.com>
2024-07-08 15:12:16 -0500

    added tests for square and circle

--- assets/lib/shapes/shapes.test.js
+++ assets/lib/shapes/shapes.test.js
@@ -1,4 +1,4 @@
-const {triangle} = require('./shapes.js');
+const {triangle, square, circle} = require('./shapes.js');
 
 describe('triangle', () => {
 
@@ -9,3 +9,23 @@ describe('triangle', () => {
         expect(testTriangle.render()).toEqual(expectedOutput);
     });
 });
+
+describe('square', () => {
+    
+    it('should render a square with a green background', () => {
+        const testSquare = new square();
+        testSquare.setColor('green');
+        const expectedOutput = '<rect width="100" height="100" fill="green"/>';
+        expect(testSquare.render()).toEqual(expectedOutput);
+    });
+});
+
+describe('circle', () => {
+        
+        it('should render a circle with a blue background', () => {
+            const testCircle = new circle();
+            testCircle.setColor('blue');
+            const expectedOutput = '<circle cx="50" cy="50" r="50" fill="blue"/>';
+            expect(testCircle.render()).toEqual(expectedOutput);
+        });
+    });
