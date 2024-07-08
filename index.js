f03343a5eafd291f82b313534e00155f4f1349f0
EliWard4 <elijah.ward014@gmail.com>
2024-07-08 13:51:27 -0500

    added an array of objects that will be used to prompt the user for input

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const {triangle, square, circle} = require('./assets/lib/shapes/shapes.js');
 
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter 3 characters for your logo',
+        name: 'logoCharacter'
+    },
+    {
+        type: 'input',
+        message: 'What color would you like for your characters',
+        name: 'charsColor'
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like for your logo',
+        choices: ['Triangle', 'Circle', 'Square'],
+        name: 'logoShape',
+    },
+    {
+        type: 'input',
+        message: 'What is your shapes color',
+        name: 'shapeColor'
+    }
+];
